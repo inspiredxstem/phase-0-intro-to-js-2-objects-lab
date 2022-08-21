70ee6fac2abbf0c21d0362e22fc1d204dbff3d3f
Steven Rosario <strosariogarcia@gmail.com>
2022-08-20 23:17:32 -0400

    commit tree 6f642c43c1c6753ad28eb79a56106c348ed922eb

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+
+const employee = {
+    name:"",
+    streetAddress: ""
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return {
+        ...obj,
+        [key]: value,
+      };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const newObj = {...obj[key]};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+}
